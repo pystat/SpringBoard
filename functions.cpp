c594e48a93cd0f6b5868d03d5800a6d4c5561415
Python & Statistics <statpy@outlook.com>
2019-03-21 06:33:20 -0400

    Add files via upload

--- functions.cpp
+++ functions.cpp
@@ -5,19 +5,48 @@
 using namespace std;
 
 void findMaxMin(const int elevations[MAX_ROWS][MAX_COLS], int rows, int cols, int& max, int& min) {
+	for(int i=0;i<rows;i++){
+		for(int j=0;j<cols;j++){
+			if(max<elevations[i][j]){
+				max=elevations[i][j];
+			}
+			if(min>elevations[i][j]){
+				min=elevations[i][j];
+			}
+		}
+	}
 }
 
 void loadData(int elevations[MAX_ROWS][MAX_COLS], int rows, int cols, istream& inData) {
+	if(inData.good()){
+		for(int i=0;i<rows;i++){
+			for(int j=0;j<cols;j++){
+				inData >> elevations[i][j];
+			}
+		}
+	}
 }
 
-int scaleValue(int value, int max, int min) {
-    return 255;
+int scaleValue(int value, int max, int min){
+	value=int(round(((value-min)*255)/((max-min)*1.0)));
+    return value;
 }
 
 void loadGreyscale(Pixel image[MAX_ROWS][MAX_COLS], const int elevations[MAX_ROWS][MAX_COLS], 
                     int rows, int cols, int max, int min){ 
+	int temp=0;
+	for(int i=0;i<rows;i++){
+		for(int j=0;j<cols;j++){
+			temp=scaleValue(elevations[i][j],max,min);
+			image[i][j].red=temp;
+			image[i][j].green=temp;
+			image[i][j].blue=temp;
+
+		}
+	}
 }
 
 void outputImage(const Pixel image[MAX_ROWS][MAX_COLS], int rows, int cols, ostream& outData) {
+
 }
 
--- main.cpp
+++ main.cpp
@@ -12,4 +12,33 @@ static int elevations[MAX_ROWS][MAX_COLS];
 static Pixel image[MAX_ROWS][MAX_COLS];
 
 int main() {
+	ifstream inData;
+	ofstream outData;
+	int rows,cols;
+	string fileName; 
+ 	int min=elevations[0][0];
+	int max=elevations[0][0];
+
+	cin>>rows;
+	if(!cin||rows<0){
+		cout << "Incorrect input" << endl;
+		return 1;
+	}
+	cin>>cols;
+	if(!cin||cols<0){
+		cout << "Incorrect input" << endl;
+		return 1;
+	}
+	cin>>fileName;
+	inData.open(fileName);
+	if (!inData.is_open()) {
+		cout << "Could not open file" << endl;
+		return 1;
+	}
+
+	
+	loadData(elevations,rows,cols,inData);
+	findMaxMin(elevations,rows,cols,max,min);
+	loadGreyscale(image, elevations, rows, cols, max, min);
+	outputImage(image, rows, cols, outData);
 }
